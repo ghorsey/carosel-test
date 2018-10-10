031ef007521448f88ef9cbb33512fff88da7e2bf
Geoff Horsey <geoff.horsey@optimityadvisors.com>
2018-10-10 10:00:21 -0700

    chore: finish impl

--- src/carousel.spa/Controllers/PicsController.cs
+++ src/carousel.spa/Controllers/PicsController.cs
@@ -6,26 +6,28 @@ namespace carousel.spa.Controllers
     using System.Threading.Tasks;
     using carousel.spa.Models;
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json.Linq;
 
     [Route("api/[controller]")]
     public class PicsController
     {
-        public async Task<List<Image>> Get() 
+        public async Task<List<Image>> Get()
         {
             var httpClient = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Get, "https://www.reddit.com/r/pics.json");
 
-
             var response = await httpClient.SendAsync(request);
 
-            var data = await response.Content.ReadAsAsync(typeof(RedditWrapper)) as RedditWrapper;
+            var data = await response.Content.ReadAsAsync(typeof(JObject)) as JObject;
 
             var result = new List<Image>();
 
-            foreach(var item  in data.Data.Children.Where(c => c.Data.Preview != null)) {
-                result.Add(new Image{
-                    Title = item.Data.Title,
-                    ImageSrc = item.Data.Preview.Images.FirstOrDefault()?.Source.Url
+            foreach (var item in data["data"]["children"].Where(i => i["data"]["preview"] != null))
+            {
+                result.Add(new Image
+                {
+                    Title = (string)item["data"]["title"],
+                    ImageSrc = (string)item["data"]["preview"]["images"][0]["source"]["url"]
                 });
             }
 
